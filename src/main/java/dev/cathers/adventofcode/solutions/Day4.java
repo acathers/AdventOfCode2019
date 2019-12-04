34701aa90f4c7d654f7956f4287b3b49d5a04100
Anthony Cathers <acathers@hawaii.edu>
2019-12-04 01:02:02 -1000

    Day 4 - clean up slightly

--- src/main/java/dev/cathers/adventofcode/solutions/Day4.java
+++ src/main/java/dev/cathers/adventofcode/solutions/Day4.java
@@ -3,23 +3,22 @@ package dev.cathers.adventofcode.solutions;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 public class Day4 extends Day {
 
     public List<Integer> solutionPartOne() {
         List<Integer> valid = new ArrayList<>();
-        for (int i = 264793; i <= 803935; i++) {
+        next:
+        for (Integer i = 264793; i <= 803935; i++) {
             int[] digits = Arrays.stream(String.valueOf(i).split("")).mapToInt(Integer::parseInt).toArray();
-            if (Arrays.stream(digits).distinct().count() != digits.length) {
-                for (int j = 0; j < 5; j++) {
-                    if (digits[j] <= digits[j + 1]) {
-                        if (j == 4) {
-                            valid.add(i);
-                        }
+            if (Arrays.stream(digits).distinct().count() == digits.length) continue;
+                for (int j = 0; j < digits.length - 1; j++) {
+                    if(digits[j] > digits[j + 1]) {
+                        continue next;
                     }
                 }
-            }
+            valid.add(i);
         }
         return valid;
     }
