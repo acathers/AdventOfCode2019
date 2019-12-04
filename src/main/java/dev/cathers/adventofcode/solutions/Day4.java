8c22d941925a4fbc498b0fed2d482662b4172298
Anthony Cathers <acathers@hawaii.edu>
2019-12-03 23:30:01 -1000

    Day 4 - clean up slightly

--- src/main/java/dev/cathers/adventofcode/solutions/Day4.java
+++ src/main/java/dev/cathers/adventofcode/solutions/Day4.java
@@ -6,27 +6,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Day4 extends Day {
-    List<Integer> valid = new ArrayList<>();
-    public List<Integer> solutionPartOne(){
-           for(int i  = 264793; i <= 803935; i++){
+
+    public List<Integer> solutionPartOne() {
+        List<Integer> valid = new ArrayList<>();
+        for (int i = 264793; i <= 803935; i++) {
             int[] digits = Arrays.stream(String.valueOf(i).split("")).mapToInt(Integer::parseInt).toArray();
-            if(Arrays.stream(digits).distinct().toArray().length ==  digits.length ) {
-                continue;
+            if (Arrays.stream(digits).distinct().count() != digits.length) {
+                for (int j = 0; j < 5; j++) {
+                    if (digits[j] <= digits[j + 1]) {
+                        if (j == 4) {
+                            valid.add(i);
+                        }
+                    }
+                }
             }
-           for(int j = 0; j < 5; j++){
-                if(digits[j] > digits[j+1]){
-                    break;
-                }
-                if(j == 4){
-                    valid.add(i);
-                }
-           }
         }
         return valid;
     }
 
-    public int solutionPartTwo(List<Integer> valid){
-        List<String> stillValid = valid.stream().map(Object::toString).filter(f -> f.matches("^((\\d)\\2(?!\\2)\\d*|\\d*(\\d)(?!\\3)(\\d)\\4(?!\\4)\\d*)$")).collect(Collectors.toList());
-        return stillValid.size();
+    public long solutionPartTwo(List<Integer> valid) {
+        return valid.stream().map(Object::toString).filter(f -> f.matches("^((\\d)\\2(?!\\2)\\d*|\\d*(\\d)(?!\\3)(\\d)\\4(?!\\4)\\d*)$")).count();
     }
 }
+
